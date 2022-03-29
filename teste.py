1ee97e1628b9baebb3f5d71cbc57523c4cbb9053
mickiedaniel <96701179+mickiedaniel@users.noreply.github.com>
2022-03-29 15:57:07 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,2 +1,11 @@
-import pandas
+a = int(input('Escreva o valor de A: '))
+b = int(input('Escreva o valor de B: '))
+#int transforma a entrada do input em int
 
+soma = a+b
+sub = a-b
+multi = a*b
+div = a/b
+
+print('A soma de A + B é  {soma}. A subtração de A + B é  {sub}. A multiplicação de A * B é  {multi}. A divisão de A / B é  {div}' .format(soma=soma,sub=sub,multi=multi,div=div))
+#format adiciona a variavel no print independente do tipo da variavel
